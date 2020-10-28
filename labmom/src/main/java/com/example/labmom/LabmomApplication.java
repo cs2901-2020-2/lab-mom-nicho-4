bdf232c8ff73c8b0227885df33a0c2fd0a02e88b
THEFLILUX <jorge.nicho@utec.edu.pe>
2020-10-28 10:52:47 -0500

    avance

--- labmom/src/main/java/com/example/labmom/LabmomApplication.java
+++ labmom/src/main/java/com/example/labmom/LabmomApplication.java
@@ -4,11 +4,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeoutException;
+
+
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class LabmomApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         SpringApplication.run(LabmomApplication.class, args);
+        Send.rabbit(args);
     }
 
 }
